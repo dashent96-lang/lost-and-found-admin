8a52326ed50f12d64204272cef014f65099f72ff
dashent96-lang <dashent96@gmail.com>
2026-03-01 21:56:07 +0100

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,18 +1,1 @@
-import { createServer } from 'http';
-import { parse } from 'url';
-import next from 'next';
 
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
-
-const port = 3000;
-
-app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url!, true);
-    handle(req, res, parsedUrl);
-  }).listen(port, () => {
-    console.log(`> Ready on http://localhost:${port}`);
-  });
-});
