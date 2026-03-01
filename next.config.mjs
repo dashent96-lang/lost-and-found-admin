bcd7a840f73de24550eb4209804a7c9973b982ce
dashent96-lang <dashent96@gmail.com>
2026-03-01 22:10:27 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  turbopack: {},
+
   images: {
     unoptimized: true,
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'api.dicebear.com',
+        protocol: "https",
+        hostname: "api.dicebear.com",
       },
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
       },
     ],
-  },
-  // This ensures that native node modules aren't bundled into the client
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-        crypto: false,
-        os: false,
-        path: false,
-        zlib: false,
-      };
-    }
-    return config;
   },
 };
 
